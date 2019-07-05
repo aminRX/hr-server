70670e827927360d69c58d902619a72647765617
Amin Ogarrio <amin@beepquest.com>
2019-07-05 00:21:14 -0700

    Added sequelice setup.

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+require('dotenv').config()
+
 require('./setup/express')(app);
+
+const database = require('./setup/database');
 
 module.exports = app;
