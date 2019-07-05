90ea1e83df0c3f641453b6a553baa58fac74daf7
Amin Ogarrio <amin@beepquest.com>
2019-07-04 21:28:28 -0700

    Var anymore.

--- test/loginTest.js
+++ test/loginTest.js
@@ -1,7 +1,7 @@
 'use strict';
-var supertest = require('supertest');
-var should = require('should');
-var server = supertest.agent('localhost:3000/v1');
+const supertest = require('supertest');
+const should = require('should');
+const server = supertest.agent('localhost:3000/v1');
 
 describe('http://localhost:3000/login', () => {
 
